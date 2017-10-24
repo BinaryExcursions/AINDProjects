5773432c1fa0cf84edbda122c37dc8950b7ef1b5
Jeff Behrbaum <sejapresident@gmail.com>
2017-10-24 00:05:55 -0400

    About to implement Only Choice algorithm

--- SudokuBoard.py
+++ SudokuBoard.py
@@ -18,11 +18,14 @@ class CSudokuBoard:
 
         self._unitList = self._rowUnits + self._colUnits + self._squareUnits
 
+
     def cross(self, a, b):
         return [s + t for s in a for t in b]
 
+
     def eliminate(self, board):
-        self.eliminateViaBox(board)
+        for index in range(1):#self.BOARD_DIM) :
+            self.eliminateViaBox(board)
 
 
     def displayBoard(self, values):
@@ -67,13 +70,19 @@ class CSudokuBoard:
                     continue
 
                 rowIndex = key[0]
-                colIndex = int(key[1]) -1
+                colIndex = int(key[1])# -1
 
                 self.removeValueOptionFromEntireBox(currBox, self._theBoard[key])
 
                 #Our Row index is a letter: 'A' - 'I'
                 self.removeValueOptionFromEntireRow(rowIndex, self._theBoard[key])
                 self.removeValueOptionFromEntireCol(colIndex, self._theBoard[key])
+
+            #once we've evaluated the entire box - now lets
+            #see where we only have a single option.
+            self.onlyChoieForBox(currBox)
+            self.onlyChoiceForRow(0)
+            self.onlyChoiceForCol(0)
 
 
     def removeValueOptionFromEntireBox(self, theBox, numValue):
@@ -83,20 +92,80 @@ class CSudokuBoard:
 
             self._theBoard[key] = self._theBoard[key].replace(numValue, "")
 
+            #Means we had two options - we removed one - now we have a
+            #known value so we need to evaluate the box again
+            if(len(self._theBoard[key]) == 1) :
+                self.removeValueOptionFromEntireBox(theBox, self._theBoard[key])
+
 
     def removeValueOptionFromEntireRow(self, rowIndex, numValue):
         for index in range(self.BOARD_DIM):
             dictKey = rowIndex + str(index + 1)
+
             if(len(self._theBoard[dictKey]) > 1) :
                 self._theBoard[dictKey] = self._theBoard[dictKey].replace(numValue, "")
+
+                # Means we had two options - we removed one - now we have a
+                # known value so we need to evaluate the row again.
+                if (len(self._theBoard[dictKey]) == 1):
+                    self.removeValueOptionFromEntireRow(rowIndex, self._theBoard[dictKey])
 
 
     #ColIndex comes in as a 0th index value
     def removeValueOptionFromEntireCol(self, colIndex, numValue):
         for index in range(self.BOARD_DIM):
-            dictKey = self.characterForRowNumber(index) + str(colIndex + 1)
+            dictKey = self.characterForRowNumber(index) + str(colIndex)# + 1)
+
             if (len(self._theBoard[dictKey]) > 1):
                 self._theBoard[dictKey] = self._theBoard[dictKey].replace(numValue, "")
+
+                # Means we had two options - we removed one - now we have a
+                # known value so we need to evaluate the column again.
+                if(len(self._theBoard[dictKey]) == 1):
+                    self.removeValueOptionFromEntireCol(colIndex, self._theBoard[dictKey])
+
+    def onlyChoieForBox(self, theBox):
+        numDict = {1:None, 2:None, 3:None, 4:None, 5:None, 6:None, 7:None, 8:None, 9:None}
+        pass
+
+    def onlyChoiceForRow(self, rowIndex):
+        numDict = {1: None, 2: None, 3: None, 4: None, 5: None, 6: None, 7: None, 8: None, 9: None}
+
+        rowChar = self.characterForRowNumber(rowIndex)
+        for index in range(self.BOARD_DIM) :
+            key = rowChar + str(rowIndex + 1)
+
+            if(len(self._theBoard[key]) == 1):
+                continue
+
+            numDict = self.incrementIndexsForOnlyChoice(numDict, self._theBoard[key], key)
+
+
+
+
+
+    def onlyChoiceForCol(self, colIndex):
+        numDict = {1: None, 2: None, 3: None, 4: None, 5: None, 6: None, 7: None, 8: None, 9: None}
+        pass
+
+
+    def incrementIndexsForOnlyChoice(self, dataDict, values, key):
+        #246 would be a length of 3 and we'd evaluate 2, 4, 6
+        for index in range(len(values)) :
+            dictKey = int(values[index])
+
+            if(dataDict[dictKey] == None):
+                dataDict[dictKey] = key
+                continue;
+
+            dataDict[dictKey] = "X" #Means it can't possibly be an ONLY choice!
+
+        return dataDict
+
+
+
+
+
 
 
     def characterForRowNumber(self, charRow):
