d21c4f4f0bef1b928368706d927a0a690561b3fe
Jeff Behrbaum <sejapresident@gmail.com>
2017-10-28 16:53:50 -0400

    Cleaned up main some

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 from SudokuBoard import CSudokuBoard
+from TwoDimBoard import C2dBoard
 
+"""
 board = CSudokuBoard();
 
 boardVals = board.grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..')
@@ -10,4 +12,16 @@ board.eliminate(boardVals)
 print("\n*******\n")
 
 board.displayBoard(boardVals)
+"""
+
+tdTest = C2dBoard();
+
+#                   123456789012345678901234567890123456789012345678901234567890123456789012345678901
+#tdTest.grid_values('..3.216..9..345..1..18764....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..')
+#tdTest.grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..')
+tdTest.grid_values ('4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......')
+
+tdTest.solveSudoku()
+tdTest.displayBoard()
+
 
